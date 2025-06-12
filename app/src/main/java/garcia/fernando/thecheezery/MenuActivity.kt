164580a8f6efc3ddf3648f5770b1a232ca32994b
DodoMCanva <106896424+DodoMCanva@users.noreply.github.com>
2025-06-12 12:58:52 +0100

    cargar

--- app/src/main/java/garcia/fernando/thecheezery/MenuActivity.kt
+++ app/src/main/java/garcia/fernando/thecheezery/MenuActivity.kt
@@ -1,6 +1,8 @@
 package garcia.fernando.thecheezery
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,14 @@ import androidx.core.view.WindowInsetsCompat
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        var btnCold : Button = findViewById(R.id.button_cold_drink) as Button
+
+        btnCold.setOnClickListener(){
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            startActivity(intent)
         }
+
     }
 }
---
+++ app/src/main/java/garcia/fernando/thecheezery/Product.kt
@@ -0,0 +1,4 @@
+package garcia.fernando.thecheezery
+
+
+data class Product(var name: String, var image: Int, var description: String, var price: Double)
